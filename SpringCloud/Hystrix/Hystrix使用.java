1770d9b8f55392c2a486cf93819f5844b1829a51
小轩 <709027500@qq.com>
2020-07-04 22:58:51 +0800

    笔记

---
+++ SpringCloud/Hystrix/Hystrix使用.java
@@ -0,0 +1,42 @@
+Hystrix一般使用在客户端 但是服务端也可以 controller  service 实现方法 反正哪里都行 
+
+因为请求慢 注意openfeign的ribbon connectionout readtimeout 时间
+	
+	
+在服务端配置 启动类加@EnableCircuitBreaker
+
+	@HystrixCommand(fallbackMethod = "timeoutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000")})
+    public String timeout(int id) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+        log.info("8001service");
+       // int a=10/0;
+        return Thread.currentThread().getName()+"timeout*****"+id;
+    }
+
+    public String timeoutHandler(int id)//注意参数 返回值类型 要和上面一样 不然无法识别
+    {
+        return Thread.currentThread().getName()+"服务器繁忙，请稍后再试!";
+    }
+	
+在客户端配置 启动类加@EnableHystrix
+yml加
+#客户端开启hystrix 要配置这个 服务端没feign 开个鸡毛
+feign:
+  hystrix:
+    enabled: true
+	
+	
+	
+	@GetMapping(value = "/consumer/pay/hystrix/timeout")
+    @HystrixCommand(fallbackMethod = "timeoutHandler",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "2500")})
+    public String timeout(int id) throws InterruptedException {
+        log.info("timeout");
+        return  paymentService.timeout(id);
+    }
+
+    public String timeoutHandler(int id)//注意参数 返回值类型 要和上面一样 不然无法识别
+    {
+        return Thread.currentThread().getName()+"客户端繁忙，请稍后再试!";
+    }
